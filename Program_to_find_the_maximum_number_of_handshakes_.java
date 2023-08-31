db980cc22a1ef15170f32c558f0c0ad48437a7e5
MRISHISADWIK <120294677+MRISHISADWIK@users.noreply.github.com>
2023-08-31 11:20:50 +0530

    import java.util.Scanner; public class handshakes{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         int s = n*(n-1)/2;         System.out.print(s);     } }

---
+++ Program_to_find_the_maximum_number_of_handshakes_.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class handshakes{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int s = n*(n-1)/2;
+        System.out.print(s);
+    }
+}
