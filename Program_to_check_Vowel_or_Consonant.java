31311e84adbd0bb01b6b5e1bffd50b726d8a7ffc
MRISHISADWIK <120294677+MRISHISADWIK@users.noreply.github.com>
2023-09-07 23:04:12 +0530

    import java.util.*; public class vowel{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         char ch = sc.nextLine().charAt(0);         String ans = (ch=='A'|| ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')? "Vowel":"Consonant";         System.out.print(ans);     } }

--- Program_to_check_Vowel_or_Consonant.java
+++ Program_to_check_Vowel_or_Consonant.java
@@ -1,15 +1,9 @@
 import java.util.*;
-public class VowelOrConsonant{
+public class vowel{
     public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
         char ch = sc.nextLine().charAt(0);
-        if(ch=='A' ||ch=='E'|| ch=='I'|| ch=='O'|| ch=='U'||ch=='a' ||ch=='e'|| ch=='i'|| ch=='o'|| ch=='u' )
-        {
-            System.out.printf("Vowel");
-        }
-        else
-        {
-            System.out.printf("Consonant");
-        }
+        String ans = (ch=='A'|| ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')? "Vowel":"Consonant";
+        System.out.print(ans);
     }
 }
