d35e8250354c328aa30d1b9681ef447a59928f65
MRISHISADWIK <120294677+MRISHISADWIK@users.noreply.github.com>
2023-09-08 19:20:49 +0530

    import java.util.*; public class CarChoice{     public static void main(String args[]){         Scanner sc = new Scanner(System.in);         int t = sc.nextInt();         for(int i=0;i<t;i++)         {             double x1 = sc.nextInt();             double x2 = sc.nextInt();             double y1 = sc.nextInt();             double y2 = sc.nextInt();             double c1 = y1/x1;             double c2 = y2/x2;             if(c1<c2)             {                 System.out.println("-1");             }             else if(c1>c2)             {                 System.out.println("1");             }             else             {                 System.out.println("0");             }         }     } }

---
+++ Car_Choice.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class CarChoice{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i=0;i<t;i++)
+        {
+            double x1 = sc.nextInt();
+            double x2 = sc.nextInt();
+            double y1 = sc.nextInt();
+            double y2 = sc.nextInt();
+            double c1 = y1/x1;
+            double c2 = y2/x2;
+            if(c1<c2)
+            {
+                System.out.println("-1");
+            }
+            else if(c1>c2)
+            {
+                System.out.println("1");
+            }
+            else
+            {
+                System.out.println("0");
+            }
+        }
+    }
+}
